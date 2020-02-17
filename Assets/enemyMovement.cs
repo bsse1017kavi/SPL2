3aa10227d99ffc7470a7f479b5f39cfd977c38a1
bsse1017kavi <bsse1017@iit.du.ac.bd>
2020-02-17 20:35:18 +0600

    Made stage 1 elements

--- Assets/enemyMovement.cs
+++ Assets/enemyMovement.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class enemyMovement : MonoBehaviour
+public class EnemyMovement : MonoBehaviour
 {
     float radius = 100f;
     float relativeX;
@@ -25,14 +25,14 @@ public class enemyMovement : MonoBehaviour
 
         if (atLeft)
         {
-            transform.Translate(Vector2.right * patrolSpeed * Time.deltaTime);
+            transform.Translate(Vector2.left * patrolSpeed * Time.deltaTime);
             this.relativeX -= patrolSpeed;
         }
 
         else if (!atLeft)
         {
             //if(relativeX>100) atLeft=true;
-            transform.Translate(Vector2.left * patrolSpeed * Time.deltaTime);
+            transform.Translate(Vector2.right * patrolSpeed * Time.deltaTime);
             this.relativeX += patrolSpeed;
         }
 
